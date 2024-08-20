a81b9815e36094d528e20c345821c1f4ace43967
DestopLine <destopline555@hotmail.com>
2024-08-19 22:00:34 -0400

    Add extra fonts to i3.sh (e.g. japanese characters)

--- scripts/scripts/archdeps/i3.sh
+++ scripts/scripts/archdeps/i3.sh
@@ -4,6 +4,6 @@ if ! command -v yay &> /dev/null; then
 	exit 1
 fi
 
-yay -S --needed git xorg i3 i3blocks xkb-switch-i3 xclip ttf-jetbrains-mono-nerd kitty rofi rofi-calc xss-lock nitrogen flameshot copyq dmenu picom network-manager-applet noto-fonts noto-fonts-emoji numix-cursor-theme adwaita-dark lxappearance alsa-utils pipewire pulseaudio yad xdotool
+yay -S --needed git xorg i3 i3blocks xkb-switch-i3 xclip ttf-jetbrains-mono-nerd kitty rofi rofi-calc xss-lock nitrogen flameshot copyq dmenu picom network-manager-applet noto-fonts noto-fonts-emoji noto-fonts-cjk numix-cursor-theme adwaita-dark lxappearance alsa-utils pipewire pulseaudio yad xdotool
 echo "Done!"
 echo "Once in a graphical session, you should run 'lxappearance' to set the themes and cursors"
