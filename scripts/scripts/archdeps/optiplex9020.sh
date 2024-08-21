fd70f7e0d376377fb45320400ef7951dafc7055d
DestopLine <destopline555@hotmail.com>
2024-08-21 17:32:39 -0400

    Add vulkan libraries to optiplex9020.sh

--- scripts/scripts/archdeps/optiplex9020.sh
+++ scripts/scripts/archdeps/optiplex9020.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 sudo systemctl enable fstrim.timer
-pacman -S --needed intel-ucode libva-intel-driver pipewire alsa-utils
+pacman -S --needed intel-ucode libva-intel-driver vulkan-intel lib32-vulkan-intel pipewire alsa-utils
