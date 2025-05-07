d0cd77d216af6829c381f2111c412ae2f799a0d6
DestopLine <destopline555@hotmail.com>
2025-05-07 19:06:06 -0400

    fix(scripts): missing hyprland arch packages

--- scripts/scripts/archdeps/hyprland.sh
+++ scripts/scripts/archdeps/hyprland.sh
@@ -4,10 +4,10 @@ if ! command -v yay &> /dev/null; then
 	exit 1
 fi
 
-yay -S --needed git hyprland hyprpaper hyprlock hyprpolkit waybar ttf-jetbrains-mono-nerd kitty rofi rofi-calc\
-	grim slurp swappy copyq dmenu wl-clipboard network-manager-applet noto-fonts noto-fonts-emoji noto-fonts-cjk\
-	numix-cursor-theme adwaita-dark lxappearance alsa-utils pipewire pulseaudio python pavucontrol blueman polkit qt5ct\
-	udiskie
+yay -S --needed git hyprland hyprpaper hyprlock hyprpolkitagent xdg-desktop-portal-hyprland waybar\
+	ttf-jetbrains-mono-nerd kitty rofi rofi-calc grim slurp swappy copyq dmenu wl-clipboard\
+	network-manager-applet noto-fonts noto-fonts-emoji noto-fonts-cjk numix-cursor-theme adwaita-dark\
+	lxappearance alsa-utils pipewire pulseaudio wireplumber python pavucontrol blueman polkit qt5ct udiskie
 
 echo "Done!"
 echo "Once in a graphical session, you should run 'lxappearance' to set the themes and cursors"
