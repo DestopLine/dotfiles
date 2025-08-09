16d5166848f0a1a957b0d4a39bdf00ab85d3f7a1
DestopLine <destopline555@hotmail.com>
2025-08-09 15:30:06 -0400

    fix(scripts): install pipewire-pulse instead of pulseaudio

--- scripts/scripts/archdeps/hyprland.sh
+++ scripts/scripts/archdeps/hyprland.sh
@@ -7,7 +7,8 @@ fi
 yay -S --needed git hyprland hyprpaper hyprlock hyprpolkitagent xdg-desktop-portal-hyprland waybar\
 	ttf-jetbrains-mono-nerd kitty rofi-wayland rofi-calc grimblast-git swappy copyq dmenu wl-clipboard\
 	network-manager-applet noto-fonts noto-fonts-emoji noto-fonts-cjk numix-cursor-theme adwaita-dark\
-	lxappearance alsa-utils pipewire pulseaudio wireplumber python pavucontrol blueman polkit qt5ct udiskie
+	lxappearance alsa-utils pipewire pipewire-pulse wireplumber python pavucontrol blueman polkit\
+	qt5ct udiskie
 
 echo "Done!"
 echo "Once in a graphical session, you should run 'lxappearance' to set the themes and cursors"
