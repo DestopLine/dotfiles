91415a77f520e0bb0690742ffb4447bfae2357b2
DestopLine <destopline555@hotmail.com>
2025-09-09 18:01:14 -0400

    feat(scripts): setup reflector in pacman.sh

--- scripts/scripts/archdeps/pacman.sh
+++ scripts/scripts/archdeps/pacman.sh
@@ -4,6 +4,15 @@ if ! command -v yay &> /dev/null; then
 	exit 1
 fi
 
-yay -S --needed pacman-contrib informant
+yay -S --needed pacman-contrib informant reflector
 sudo systemctl enable paccache.timer
+sudo systemctl enable reflector.timer
+
+sudo tee /etc/xdg/reflector/reflector.conf << EOF
+--save /etc/pacman.d/mirrorlist
+--protocol https
+--latest 5
+--sort rate
+EOF
+
 echo "Add your user to the informant group with 'sudo usermod -aG informant <username>'"
